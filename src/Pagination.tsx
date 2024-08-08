29940da41faa978e97e40df811e19ec0038d8596
kailam-ada <kai_lam@hotmail.fr>
2024-08-08 15:12:13 +0200

    new style

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -25,7 +25,22 @@ function Pagination({
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
+        className={currentPage === 1 ? "disabled" : ""}
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
+        </svg>
         Previous
       </button>
       {pageNumbers.map((number) => (
@@ -40,8 +55,23 @@ function Pagination({
       <button
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
+        className={currentPage === totalPages ? "disabled" : ""}
       >
         Next
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+          />
+        </svg>
       </button>
       <select
         value={itemsPerPage}
